f033236a9c15aa7dd110eb10f902021445f90997
Ricardo Gomez <rgomez@gatech.edu>
2019-09-02 18:10:19 -0400

    Initial Commit

---
+++ ios-programming-assignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios-programming-assignment
+//
+//  Created by Ricky Gomez on 9/2/19.
+//  Copyright © 2019 Ricky Gomez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
